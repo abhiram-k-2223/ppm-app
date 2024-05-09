c52c82f02668fcd6470b5b1736fb59b3b907311b
Abhiram Kamsali <148453682+abhiram-k-2223@users.noreply.github.com>
2024-05-09 20:45:41 +0530

    Add files via upload

---
+++ new-rec.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+
+with open(r'C:\Users\LENOVO T480\Desktop\ㅤ\model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+cgpa = st.number_input("Enter your CGPA")
+iq = st.number_input("Enter your IQ")
+
+if st.button("Submit"):
+    prediction = model.predict([[cgpa, iq]])
+    if(prediction == 1):
+        st.write("congrats man you got placed")
+    else:
+        st.write("sorry mate you didn't get placed")
